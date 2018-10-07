a114ed2b904b296f29877c71f2006dbe1f863eae
Charlotte Prevost <chaprevost91@gmail.com>
2018-10-07 18:16:30 -0500

    made class Car

---
+++ Car.js
@@ -0,0 +1,29 @@
+// In the Car.js file:
+
+// Users should be able to define the following properties of a new car:
+
+// color
+// convertible (boolean)
+// Cars should automatically come with the following property:
+
+// speed (0, at first)
+// Functions specs:
+
+// include accelerate and decelerate
+// these should take one argument, the speed, and add or substract it the from the current speed
+
+class Car {
+	constructor (color, convertible){
+		this.color = color;
+		this.convertible = convertible;
+		this.speed = 0;
+	}
+
+	accelerate(a){
+		this.speed+=a;
+	}
+
+	decelerate(d){
+		this.speed-=d;
+	}
+}
