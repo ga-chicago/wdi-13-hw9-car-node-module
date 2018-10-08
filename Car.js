95eb1cad574f037c32efe513ca15a37152b4a474
Andy Prudhomme <aprudhomme9@gmail.com>
2018-10-08 09:27:39 -0500

    fixed methods and syntax errors, tests working

--- Car.js
+++ Car.js
@@ -3,12 +3,12 @@ module.exports = class Car {
 		this.color = color;
 		this.convertible = convertible;
 		this.speed = 0;
-	},
+	}
 	decelerate(speed) {
 		this.speed -= speed;
-	},
+	}
 	accelerate(speed) {
-		this.speed -= speed;
+		this.speed += speed;
 	}
 
 }
