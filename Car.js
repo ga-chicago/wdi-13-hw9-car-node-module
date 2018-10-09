c0e1e36e7bfa34914f2cfec2d39d2f2ef237df8b
Ashley <ashley@Jamess-MacBook-Air.local>
2018-10-09 14:03:06 -0500

    test

---
+++ Car.js
@@ -0,0 +1,18 @@
+
+
+//car
+
+module.exports = class Car {
+	constructor(color, convertible) {
+		this.color = color;
+		this.convertible = convertible;
+		this.speed = 0;
+	}
+	accelerate(number) {
+		this.speed+= number;
+	}
+	decelerate(number) {
+		this.speed-= number;
+	}
+
+}
